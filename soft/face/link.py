dae6ca74a632601dcfc55212104c375cdc873702
barthess <barthess@yandex.ru>
2012-06-04 16:16:05 +0300

    added serial proxy process

--- soft/face/link.py
+++ soft/face/link.py
@@ -7,25 +7,27 @@ import ConfigParser
 import sys
 import os
 import socket
-
 from struct import *
+import serial
+from binascii import hexlify
+
 from multiprocessing import Queue
 from multiprocessing import Event
 from Queue import Empty, Full # для отлова исключений
-import serial
-import io
-from binascii import hexlify
 
 from utils import *
-
 import globalflags
 flags = globalflags.flags
+
+# read settings from file
+config = ConfigParser.SafeConfigParser()
+config.read('default.cfg')
 
 # allow import from the parent directory, where mavlink.py and its stuff are
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '../mavlink/python'))
 import mavlink
 
-
+# fifo object for mavlink
 class fifo(object):
     def __init__(self):
         self.buf = []
@@ -38,31 +40,54 @@ class fifo(object):
 f = fifo() # buffer for mavlink
 mav = mavlink.MAVLink(f)
 
+# Socket settings
+ADDR = "localhost", config.getint("Socket", "PORT_UDP_HUD")
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((ADDR))
+
+# файл для записи ошибок
+errlog = open('logs/link.log', 'a')
 
 
-def linkin(q_tlm, q_log, e_pause, e_kill, config):
-    """
-    менеджер сообщений
-    Принимает поток данных, выискивает пакеты, сортирует.
-
-    """
-    # UDP сокет для всех желающих
-    # SOCK_DGRAM is the socket type to use for UDP sockets
-    port_udp_agc = config.getint('Link', 'PORT_UDP_QGC')
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # файл для записи ошибок
-    errlog = open('logs/link.log', 'a')
-
-    #настройка верёвки для связи по ком-порту
-    baudrate = config.getint('Link', 'baudrate')
-    port     = config.get('Link', 'port')
-    ser = serial.Serial(port, baudrate, timeout = 1)
-    # TODO: create mpiovd presence detector based on empty string returned on
-    # timeout
-    sio = io.BufferedRWPair(ser, ser, 1024)
+def __receive_data(q_tlm, q_log):
+    """ Парсит байты из сетевого сокета в сообщения """#{{{
     m = None
+    c = sock.recv(1024)
 
+    try:
+        m = mav.parse_char(c)
+        # print hexlify(c)
+    except mavlink.MAVError:
+        pass
+
+    if m != None:
+        # print hexlify(m)
+        # if type(m) == mavlink.MAVLink_mpiovd_sensors_scaled_message:
+        #     try:
+        #         q_tlm.put_nowait(m)
+        #     except Full:
+        #         errlog.write(str(datetime.datetime.now()) + " -- Telemetry queue is full\n")
+        #     dbgprint(m)
+        #     m = None
+
+        if type(m) == mavlink.MAVLink_mpiovd_sensors_raw_message:
+            try:
+                q_tlm.put_nowait(m)
+            except Full:
+                errlog.write(str(datetime.datetime.now()) + " -- Telemetry queue is full\n")
+            dbgprint(m)
+            m = None
+
+        elif type(m) == mavlink.MAVLink_heartbeat_message:
+            dbgprint(m)
+            m = None
+
+        else:
+            # dbgprint("I do not how to handle message of type " + str(type(m)) + " .")
+            m = None
+#}}}
+def linkin(q_tlm, q_log, e_pause, e_kill, config):
+    """ Менеджер входящих сообщений. """ #{{{
     # ждем, пока нас снимут с паузы
     dbgprint("**** link input thread ready")
     e_pause.wait()
@@ -72,44 +97,9 @@ def linkin(q_tlm, q_log, e_pause, e_kill, config):
         if e_kill.is_set():
             dbgprint("**** Link input thread. Kill signal received. Exiting")
             return
-        # c = sio.read()
-        c = ser.read()
-        # send byte via udp
-        sock.sendto(c, ("localhost", port_udp_agc))
-
-        try:
-            m = mav.parse_char(c)
-            # print hexlify(c)
-        except mavlink.MAVError:
-            pass
-
-        if m != None:
-            # print hexlify(m)
-            # if type(m) == mavlink.MAVLink_mpiovd_sensors_scaled_message:
-            #     try:
-            #         q_tlm.put_nowait(m)
-            #     except Full:
-            #         errlog.write(str(datetime.datetime.now()) + " -- Telemetry queue is full\n")
-            #     dbgprint(m)
-            #     m = None
-
-            if type(m) == mavlink.MAVLink_mpiovd_sensors_raw_message:
-                try:
-                    q_tlm.put_nowait(m)
-                except Full:
-                    errlog.write(str(datetime.datetime.now()) + " -- Telemetry queue is full\n")
-                dbgprint(m)
-                m = None
-
-            elif type(m) == mavlink.MAVLink_heartbeat_message:
-                dbgprint(m)
-                m = None
-
-            else:
-                # dbgprint("I do not how to handle message of type " + str(type(m)) + " .")
-                m = None
-
-
+        else:
+            __receive_data(q_tlm, q_log)
+#}}}
 
 #def linkout(q_out, e_pause, e_kill):
 #    """
---
+++ soft/face/serialproxy.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+import socket
+import time
+import datetime
+import serial
+from binascii import hexlify
+
+from utils import *
+import globalflags
+flags = globalflags.flags
+
+#
+errlog = open('logs/proxy.log', 'a')
+
+# load settings from config file
+config = ConfigParser.SafeConfigParser()
+config.read('default.cfg')
+
+# port list from config
+portlist = []
+for i in config.items("Socket"):
+    portlist.append(config.getint("Socket", i[0]))
+
+# SOCK_DGRAM is the socket type to use for UDP sockets
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# serial port
+bufsize  = config.getint('Serial', 'bufsize')
+baudrate = config.getint('Serial', 'baudrate')
+port     = config.get('Serial', 'port')
+ser      = serial.Serial(port, baudrate, timeout = 0.5)
+
+# infinitely read serial port and push data to sockets
+while True:
+    c = ser.read(bufsize)
+    if len(c) != 0:
+        for i in portlist:
+            sock.sendto(c, ("localhost", i))
+    else:
+        errlog.write(str(datetime.datetime.now()) + " -- connection interrupted\n")
+
+
