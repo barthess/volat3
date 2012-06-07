6793f715342cd89d0dfd3fe4bab8ba7ce633246c
barthess <barthess@yandex.ru>
2012-06-07 14:55:35 +0300

    some changes in gui

--- soft/face/tuner_qt.py
+++ soft/face/tuner_qt.py
@@ -4,22 +4,61 @@
 import struct
 import sys
 import time
+import collections
+import user
 
 from Queue import Empty # для отлова исключений
 from multiprocessing import Queue, Event
 
-from PyQt4.QtCore import QObject
-from PyQt4.QtGui import QApplication, QDialog
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph.parametertree.parameterTypes as pTypes
+from pyqtgraph.parametertree import Parameter, ParameterTree, ParameterItem, registerParameterType
 from PyQt4 import uic
 
-def refresh():
-    print "refresh clicked"
+app = QtGui.QApplication(sys.argv)
 
-app = QApplication(sys.argv)
-
+f = open("tuner_ui.py", "w")
+uic.compileUi("tuner.ui", f)
 tuner = uic.loadUi("tuner.ui")
-tuner.buttonRefersh.clicked.connect(refresh)
 
+
+
+p0 = {'name': 'Param 1', 'type': 'int', 'value': 10}
+p1 = {'name': 'Param 2', 'type': 'float', 'value': 10}
+
+g0 = {'name': 'Group 0', 'type': 'group', 'children': [p0, p1]}
+g1 = {'name': 'Group 1', 'type': 'group', 'children': [p0, p1]}
+
+params = []
+p = Parameter(name='params', type='group', children=params)
+t = ParameterTree(parent=tuner.tabRawTree)
+t.setParameters(p, showTop=False)
+# print tuner.tabRawTree.width()
+# t.resize(400, 500)
+
+
+
+def refresh():#{{{
+    print "refresh clicked"
+    tuner.labelStatusBar.setText("loading values")
+    params.append(g0)
+    params.append(g1)
+    global p, t
+    p = Parameter(name='params', type='group', children=params)
+    t.setParameters(p, showTop=False)
+#}}}
+
+
+
+
+
+
+
+
+
+
+
+tuner.buttonRefersh.clicked.connect(refresh)
 
 tuner.show()
 sys.exit(app.exec_())
