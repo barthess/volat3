2f8ad1e3b3a2c14ee6f89b738ffd00a168ac0e2e
barthess <barthess@yandex.ru>
2012-07-06 17:05:39 +0300

    nop

--- soft/firmware/src/i2c_local.c
+++ soft/firmware/src/i2c_local.c
@@ -26,7 +26,7 @@ extern uint32_t GlobalFlags;
 /* interface #2 */
 static const I2CConfig i2cfg2 = {
     OPMODE_I2C,
-    400000, //100000, //
+    360000, //100000, //
     FAST_DUTY_CYCLE_16_9, //STD_DUTY_CYCLE, //
 };
 
