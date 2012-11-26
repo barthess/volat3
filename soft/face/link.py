d9b57527bbb9c53a51288cfdc64a71c5c74c99b4
barthess <barthess@yandex.ru>
2012-11-26 19:08:29 +0300

    nop

--- soft/face/link.py
+++ soft/face/link.py
@@ -42,7 +42,7 @@ def input(q, e_pause, e_kill, device):#{{{
         except socket.timeout: pass
 
         if m is not None:
-            print type(m)
+            # print type(m)
             try: q.put_nowait(m)
             except Full: pass
 #}}}
