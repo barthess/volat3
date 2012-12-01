b9d7c268ba4bf28f5c6df2158f4ab59792cf22cf
barthess <barthess@yandex.ru>
2012-12-01 15:16:51 +0300

    saved working unpackers

--- soft/bnap/src/link/link_dm_unpacker.c
+++ soft/bnap/src/link/link_dm_unpacker.c
@@ -3,9 +3,10 @@
 #include "mavlink.h"
 
 #include "link.h"
-#include "link_dm.h"
 #include "message.h"
 #include "main.h"
+
+#include "link_dm.h"
 
 /*
  ******************************************************************************
@@ -14,19 +15,23 @@
  */
 extern GlobalFlags_t GlobalFlags;
 
-extern mavlink_system_t                mavlink_system_struct;
+extern mavlink_system_t                   mavlink_system_struct;
 
-extern mavlink_command_long_t          mavlink_command_long_struct;
-extern mavlink_param_set_t             mavlink_param_set_struct;
-extern mavlink_param_request_list_t    mavlink_param_request_list_struct;
-extern mavlink_param_request_read_t    mavlink_param_request_read_struct;
-extern mavlink_heartbeat_t             mavlink_heartbeat_dm_struct;
+extern mavlink_command_long_t             mavlink_command_long_struct;
+extern mavlink_param_set_t                mavlink_param_set_struct;
+extern mavlink_param_request_list_t       mavlink_param_request_list_struct;
+extern mavlink_param_request_read_t       mavlink_param_request_read_struct;
+extern mavlink_heartbeat_t                mavlink_heartbeat_dm_struct;
+extern mavlink_oblique_storage_request_count_t mavlink_oblique_storage_request_count_struct;
+extern mavlink_oblique_storage_request_t  mavlink_oblique_storage_request_struct;
 
 extern EventSource event_mavlink_command_long;
 extern EventSource event_mavlink_param_set;
 extern EventSource event_mavlink_param_request_list;
 extern EventSource event_mavlink_param_request_read;
 extern EventSource event_mavlink_heartbeat_dm;
+extern EventSource event_mavlink_oblique_storage_request_count;
+extern EventSource event_mavlink_oblique_storage_request;
 
 /*
  *******************************************************************************
@@ -41,6 +46,7 @@ void DmUnpackCycle(SerialDriver *sdp){
   mavlink_message_t msg;
   mavlink_status_t status;
   msg_t c = 0;
+  msg_t prev_c = 0;
 
   while(GlobalFlags.messaging_ready == 0)
     chThdSleepMilliseconds(50);
@@ -51,6 +57,7 @@ void DmUnpackCycle(SerialDriver *sdp){
       continue;
     }
     else{
+      (void)prev_c;
       c = sdGetTimeout((SerialDriver *)sdp, MS2ST(50));
       if (c == Q_TIMEOUT)
         continue;
@@ -81,6 +88,19 @@ void DmUnpackCycle(SerialDriver *sdp){
         if (mavlink_param_request_read_struct.target_system == mavlink_system_struct.sysid)
           chEvtBroadcastFlags(&event_mavlink_param_request_read, EVMSK_MAVLINK_PARAM_REQUEST_READ);
         break;
+
+
+
+      case MAVLINK_MSG_ID_OBLIQUE_STORAGE_REQUEST:
+        mavlink_msg_oblique_storage_request_decode(&msg, &mavlink_oblique_storage_request_struct);
+        if (mavlink_oblique_storage_request_struct.target_system == mavlink_system_struct.sysid)
+          chEvtBroadcastFlags(&event_mavlink_oblique_storage_request, EVMSK_MAVLINK_OBLIQUE_STORAGE_REQUEST);
+        break;
+
+
+
+
+
 
       case MAVLINK_MSG_ID_HEARTBEAT:
         mavlink_msg_heartbeat_decode(&msg, &mavlink_heartbeat_dm_struct);
